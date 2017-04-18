5ab6a4dc4862d44b41f1dfc21120d076f07e933d
Catarina Cepeda <catarinacepeda@gmail.com>
2017-04-18 14:58:14 +0100

    Fix MetricMethodCount

---
+++ RenderServer/src/raytracer/MetricMethodCount.java
@@ -0,0 +1,81 @@
+import BIT.highBIT.ClassInfo;
+import BIT.highBIT.Routine;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.logging.*;
+
+/**
+ * Created by Catarina on 17/04/2017.
+ */
+public class MetricMethodCount {
+
+    private static Logger logger = Logger.getLogger(MetricMethodCount.class.getName());
+    private static int m_count = 0;
+
+    public static void main(String[] args) {
+
+        LogManager.getLogManager().reset();
+        Logger rootLogger = LogManager.getLogManager().getLogger("");
+        rootLogger.setUseParentHandlers(false);
+        Handler fileHandler = null;
+        try {
+            fileHandler = new FileHandler("metrics.log", true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        fileHandler.setFormatter(new SimpleFormatter());
+        rootLogger.addHandler(fileHandler);
+
+        String outputFolder = args[1];
+
+        if (args[0].endsWith(".class")) {
+            if (!args[0].contains(System.getProperty("file.separator"))) {
+                instrumentClass(args[0], ".", outputFolder);
+            } else {
+                int index = args[0].lastIndexOf(System.getProperty("file.separator"));
+                String sourceFolder = args[0].substring(0, index);
+                String classFile = args[0].substring(index + 1);
+                instrumentClass(classFile, sourceFolder, outputFolder);
+            }
+        } else {
+            File file_in = new File(args[0]);
+            String infilenames[] = file_in.list();
+            if (infilenames == null) {
+                return; //is not a directory
+            }
+            for (int i = 0; i < infilenames.length; i++) {
+                String infilename = infilenames[i];
+                if (infilename.endsWith(".class")) {
+                    instrumentClass(infilename, args[0], outputFolder);
+                }
+            }
+        }
+    }
+
+    private static synchronized void instrumentClass(String filename, String srcFolder, String destFolder) {
+        // create class info object
+        ClassInfo ci;
+        ci = new ClassInfo(srcFolder + System.getProperty("file.separator") + filename);
+
+        // loop through all the routines
+        for (Enumeration e = ci.getRoutines().elements(); e.hasMoreElements(); ) {
+            Routine routine = (Routine) e.nextElement();
+            routine.addBefore("MetricMethodCount", "mcount", new Integer(1));
+            if (routine.getMethodName().contentEquals("draw")) {
+                routine.addAfter("MetricMethodCount", "storeMethodCount", ci.getClassName());
+            }
+        }
+//        ci.addAfter("MetricMethodCount", "storeMethodCount", ci.getClassName());
+        ci.write(destFolder + System.getProperty("file.separator") + filename);
+    }
+
+    public static synchronized void storeMethodCount(String foo) {
+        logger.info(Thread.currentThread().getName() + " Methods executed: " + m_count);
+    }
+
+    public static synchronized void mcount(int incr) {
+        m_count += incr;
+    }
+}
