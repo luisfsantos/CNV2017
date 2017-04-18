e3e9ecbdfdef8f0ce47dea30ff688b50cadee506
Catarina Cepeda <catarinacepeda@gmail.com>
2017-04-18 15:08:02 +0100

    fix RenderServer scripts

--- RenderServer/render.bat
+++ RenderServer/render.bat
@@ -1,6 +1,7 @@
 REM the line below makes sure it starts in the directory where the script resides
 cd %~dp0
 
+if not exist src\raytracer\instrumented mkdir src\raytracer\instrumented
 javac -cp src;lib src\raytracer\*.java src\raytracer\pigments\*.java src\raytracer\shapes\*.java
 java -cp src;lib;src\raytracer MetricMethodCount src\raytracer\RayTracer.class src\raytracer\instrumented
 move src\raytracer\instrumented\RayTracer.class src\raytracer\
--- RenderServer/render.sh
+++ RenderServer/render.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd "${0%/*}" # make sure it starts in the directory where the script resides
 
+if [! -d src/raytracer/instrumented]; then
+	mkdir src/raytracer/instrumented
+fi
 javac -cp src:lib src/raytracer/*.java src/raytracer/pigments/*.java src/raytracer/shapes/*.java
 java -cp src:lib:src/raytracer MetricMethodCount src/raytracer/RayTracer.class src/raytracer/instrumented
 mv src/raytracer/instrumented/RayTracer.class src/raytracer/
